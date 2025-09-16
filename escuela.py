51e5c2ffac578acf415842771df0ef3844f56d72
chavezcjcg <cristianchavezcjcg@gmail.com>
2025-09-16 17:37:09 -0600

    Actualizacion de roles del sistema

--- escuela.py
+++ escuela.py
@@ -35,45 +35,6 @@ class Instructor(Usuario): #Se hereda de la clase padre
         self.especialidad = especialidad
         self.curso = set() #Se verifica que el instructor no se duplique en el mismo curso
 
-class Curso: #Se define una clase padre
-    def __init__(self, codigo, nombre, instructor): #Constructor inicial
-        if not isinstance (instructor,Instructor): #Se verifica que el instructor ya haya sido creaddo antes
-            raise TypeError ("Debe de haber un instructor valido")
-        self.codigo = codigo
-        self.nombre = nombre
-        self.instructor = instructor
-        self.estudiantes = {}
-        self.evaluaciones = {}
-        self.calificaciones = {}
-
-    def inscribir_estudiantes(self,estudiante): #Se crea un metodo para poder inscribir a los estudiantes
-        if estudiante.id in self.estudiantes: #En caso de que el 'id' del estudiante este en el diccionario este retornara un mensaje de invaliddo
-            raise ValueError("El estudiante ya ha sido inscrito en este curso")
-        self.estudiantes[estudiante.id] = estudiante
-        estudiante.cursos_inscritos.add(self.codigo) #Se guarda datos del curso en Estudiante, donde guardamos el codigo del Curso
-
-    
-
-
-# Creamos un instructor
-carlos = Instructor(1, "Carlos", "carlos@mail.com", "Matemáticas")
-
-# Creamos un curso con ese instructor
-curso_mate = Curso("MAT101", "Matemáticas I", carlos)
-
-# Creamos un estudiante
-ana = Estudiante(2, "Ana", "ana@mail.com", "2025-123")
-
-# Inscribimos a la estudiante en el curso
-curso_mate.inscribir_estudiantes(ana)
-
-# --- Pruebas ---
-print("Instructor del curso:", curso_mate.instructor.nombre)
-print("Estudiantes inscritos en el curso:", [e.nombre for e in curso_mate.estudiantes.values()])
-print("Cursos de la estudiante Ana:", ana.cursos_inscritos)
-
-# Intentar inscribirla otra vez (debería lanzar error)
-try:
-    curso_mate.inscribir_estudiantes(ana)
-except ValueError as e:
-    print("Error al reinscribir:", e)
+class Admin(Usuario):
+    def __init__(self, id, nombre, email):
+        super().__init__(id, nombre, email)
